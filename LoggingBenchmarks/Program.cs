fc093b9c2e2719c098a3ddda279d88c5e8d5db35
Steve Gordon <sgordon@hotmail.co.uk>
2019-07-11 13:43:43 +0100

    Initial commit

---
+++ LoggingBenchmarks/Program.cs
@@ -0,0 +1,85 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using JetBrains.Profiler.Api;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingBenchmarks
+{
+    internal class Program
+    {
+        private static void Main() => _ = BenchmarkRunner.Run<LoggingBenchmarks>();
+
+        //private static void Main()
+        //{
+        //    IServiceCollection serviceCollection = new ServiceCollection();
+        //    serviceCollection.AddLogging(builder => builder
+        //        .AddFilter(level => level >= LogLevel.Information)
+        //    );
+
+        //    var loggerFactory = serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();
+
+        //    var logger = loggerFactory.CreateLogger("TEST");
+
+        //    var sut1 = new SampleClassWhichLogsOriginal(logger);
+        //    var sut2 = new SampleClassWhichLogs(logger);
+
+        //    const string hello = "Hello";
+        //    const int ten = 10;
+
+        //    MemoryProfiler.CollectAllocations(true);
+            
+        //    MemoryProfiler.GetSnapshot();
+
+        //    sut1.LogOnce(hello, ten);
+
+        //    MemoryProfiler.GetSnapshot();
+
+        //    sut2.DoSomethingWhichLogsOftenUsingLoggerMessage(hello, ten);
+
+        //    MemoryProfiler.GetSnapshot();
+        //}
+    }
+
+    [MemoryDiagnoser]
+    public class LoggingBenchmarks
+    {
+        private SampleClassWhichLogsOriginal _sut1;
+        private SampleClassWhichLogs _sut2;
+
+        private const string Value1 = "Value";
+        private const int Value2 = 1000;
+
+        private ILogger _logger;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddLogging(builder => builder
+                .AddFilter(level => level >= LogLevel.Information)
+            );
+
+            var loggerFactory = serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();
+
+            var logger = loggerFactory.CreateLogger("TEST");
+            _logger = logger;
+
+            _sut1 = new SampleClassWhichLogsOriginal(logger);
+            _sut2 = new SampleClassWhichLogs(logger);
+        }
+
+        [Benchmark(Baseline = true)]
+        public void LogDirect() => _sut1.DoSomethingWhichLogsOften(Value1, Value2);
+
+        [Benchmark]
+        public void LogViaStatic() => _sut2.DoSomethingWhichLogsOftenUsingLoggerMessage(Value1, Value2);
+
+        //[Benchmark]
+        //public void LogDebugWithoutLevelCheck() => _sut2.DoSomethingWhichLogsOftenUsingLoggerMessageWithoutLevelCheck(Value1, Value2);
+
+        //[Benchmark]
+        //public void LogDebugWithLevelCheck() => _sut2.DoSomethingWhichLogsOftenUsingLoggerMessageWithLevelCheck(Value1, Value2);
+    }
+}
---
+++ LoggingBenchmarks/SampleClassWhichLogs.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingBenchmarks
+{
+    public class SampleClassWhichLogs
+    {
+        private readonly ILogger _logger;
+
+        public SampleClassWhichLogs(ILogger logger) => _logger = logger;
+
+        public void DoSomethingWhichLogsOftenUsingLoggerMessage(string value1, int value2)
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                Log.InformationalMessage(_logger, value1, value2);
+            }
+        }
+
+        public void DoSomethingWhichLogsOftenUsingLoggerMessageWithoutLevelCheck(string value1, int value2)
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                Log.DebugMessage(_logger, value1, value2);
+            }
+        }
+
+        public void DoSomethingWhichLogsOftenUsingLoggerMessageWithLevelCheck(string value1, int value2)
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                Log.DebugMessageWithLevelCheck(_logger, value1, value2);
+            }
+        }
+
+        public static class Log
+        {
+            public static class Events
+            {
+                public static readonly EventId Started = new EventId(100, "Started");
+            }
+
+            private static readonly Action<ILogger, string, int, Exception> _informationLoggerMessage = LoggerMessage.Define<string, int>(
+                LogLevel.Information,
+                Log.Events.Started,
+                "This is a message with two params! {Param1}, {Param2}");
+
+            public static void InformationalMessage(ILogger logger, string value1, int value2)
+            {
+                _informationLoggerMessage(logger, value1, value2, null);
+            }
+
+            private static readonly Action<ILogger, string, int, Exception> _debugLoggerMessage = LoggerMessage.Define<string, int>(
+                LogLevel.Debug,
+                Log.Events.Started,
+                "This is a message with two params! {Param1}, {Param2}");
+
+            public static void DebugMessage(ILogger logger, string value1, int value2)
+            {
+                _debugLoggerMessage(logger, value1, value2, null);
+            }
+
+            public static void DebugMessageWithLevelCheck(ILogger logger, string value1, int value2)
+            {
+                if (logger.IsEnabled(LogLevel.Debug))
+                    _debugLoggerMessage(logger, value1, value2, null);
+            }
+        }
+    }
+
+    public class SampleClassWhichLogsOriginal
+    {
+        private readonly ILogger _logger;
+
+        public SampleClassWhichLogsOriginal(ILogger logger) => _logger = logger;
+
+        public void LogOnce(string value1, int value2)
+        {
+            _logger.LogInformation("This is a message with two params! {Param1}, {Param2}", value1, value2);
+        }
+
+        public void DoSomethingWhichLogsOften(string value1, int value2)
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                _logger.LogInformation("This is a message with two params! {Param1}, {Param2}", value1, value2);
+            }
+        }
+    }
+}

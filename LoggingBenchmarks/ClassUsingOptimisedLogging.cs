3abcd2ee582e17c5261dbc7e75e1d843c9313a69
Steve Gordon <sgordon@hotmail.co.uk>
2019-11-14 12:55:36 +0000

    Tweaking structure

--- LoggingBenchmarks/ClassUsingOptimisedLogging.cs
+++ LoggingBenchmarks/ClassUsingOptimisedLogging.cs
@@ -21,31 +21,31 @@ namespace LoggingBenchmarks
 
         public void LogOnceUsingLoggerMessageWithLevelCheck(string value1, int value2) => Log.DebugMessageWithLevelCheck(_logger, value1, value2);
 
-        public static class Log
+        internal static class Events
         {
-            public static class Events
-            {
-                public static readonly EventId Started = new EventId(100, "Started");
-            }
+            public static readonly EventId Started = new EventId(100, "Started");
+        }
 
+        private static class Log
+        {
             private static readonly Action<ILogger, Exception> _informationLoggerMessageNoParams = LoggerMessage.Define(
                 LogLevel.Information,
-                Log.Events.Started,
+                Events.Started,
                 "This is a message with no params!");
 
             private static readonly Action<ILogger, string, Exception> _informationLoggerMessageOneParam = LoggerMessage.Define<string>(
                 LogLevel.Information,
-                Log.Events.Started,
+                Events.Started,
                 "This is a message with one param! {Param1}");
 
             private static readonly Action<ILogger, string, int, Exception> _informationLoggerMessage = LoggerMessage.Define<string, int>(
                 LogLevel.Information,
-                Log.Events.Started,
+                Events.Started,
                 "This is a message with two params! {Param1}, {Param2}");
             
             private static readonly Action<ILogger, string, int, Exception> _debugLoggerMessage = LoggerMessage.Define<string, int>(
                 LogLevel.Debug,
-                Log.Events.Started,
+                Events.Started,
                 "This is a debug message with two params! {Param1}, {Param2}");
 
             public static void InformationalMessageNoParams(ILogger logger) => _informationLoggerMessageNoParams(logger, null);
